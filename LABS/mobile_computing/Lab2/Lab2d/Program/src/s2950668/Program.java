dc25d21526801f6b2e2b81f62c1bfb1806b5424e
Nyxzore <damian.l.nel@gmail.com>
2026-02-27 10:36:45 +0200

    2d

---
+++ LABS/mobile_computing/Lab2/Lab2d/Program/src/s2950668/Program.java
@@ -0,0 +1,15 @@
+package s2950668;
+import java.util.Scanner;
+public class Program {
+	public static void main(String args []){
+	int[] arr = new int[10]; 
+	Scanner in = new Scanner(System.in);
+	for(int i=0; i<arr.length; i++) {
+		arr[i] = Integer.parseInt(in.nextLine());
+	}
+	int indx = Integer.parseInt(in.nextLine());
+	System.out.print(arr[indx]);
+	
+	in.close();
+	}
+}
